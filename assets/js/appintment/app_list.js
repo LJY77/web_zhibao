14be298c23c83bcadd677bf2047e0e2726368e51
seven <1421469384@qq.com>
2021-01-03 12:57:12 +0800

    修改了质保服务功能

--- assets/js/appintment/app_list.js
+++ assets/js/appintment/app_list.js
@@ -28,7 +28,7 @@ $(function () {
     // 需要将请求参数对象提交到服务器
     var q = {
         pagenum: 1, // 页码值，默认请求第一页的数据
-        pagesize: 2, // 每页显示几条数据，默认每页显示2条
+        pagesize: 4, // 每页显示几条数据，默认每页显示2条
         cate_id: '', // 文章分类的 Id
         state: '' // 文章的发布状态
     }
@@ -43,7 +43,7 @@ $(function () {
             data: q,
             success: function (res) {
                 if (res.status !== 0) {
-                    return layer.msg('获取文章列表失败！')
+                    return layer.msg('获取质保服务列表失败！')
                 }
                 // 使用模板引擎渲染页面的数据
                 var htmlStr = template('tpl-table', res)
